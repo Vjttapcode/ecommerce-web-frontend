c965690bf0f629f298db5a3c2059711fd3a9fcff
Vjttapcode <ducvn180@gmail.com>
2025-10-16 23:50:54 +0700

    Update Search products by Category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from "@angular/common/http";
 import { Product } from "../common/product";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { ProductCategory } from "../common/product-category";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
+  private categoryUrl = "http://localhost:8080/api/product-category";
   private baseUrl = "http://localhost:8080/api/products";
 
   constructor(private httpClient: HttpClient) {}
@@ -16,13 +18,25 @@ export class ProductService {
     //need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.httpClient
-      .get<GetResponse>(searchUrl)
+      .get<GetResponseProducts>(searchUrl)
       .pipe(map((response) => response._embedded.products));
   }
-}
 
-interface GetResponse {
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductsCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
+  }
+}
+//unwrap the JSON from Spring Data REST _embedded entry
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+}
+//unwrap the JSON from Spring Data REST _embedded entry
+interface GetResponseProductsCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
